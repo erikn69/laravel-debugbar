73d06bf7243f016bfb86e1bef1a8788fa98e80ac
erikn69 <erikn_69@hotmail.com>
2025-05-02 11:05:07 -0500

    upgrades

--- src/DataCollector/CacheCollector.php
+++ src/DataCollector/CacheCollector.php
@@ -47,6 +47,7 @@ class CacheCollector extends TimeDataCollector
         parent::__construct($requestStartTime);
 
         $this->collectValues = $collectValues;
+        $this->memoryMeasure = true;
     }
 
     public function onCacheEvent($event)
@@ -56,6 +57,10 @@ class CacheCollector extends TimeDataCollector
         $label = $this->classMap[$class][0];
 
         if (isset($params['value'])) {
+            $params['memoryUsage'] = strlen(serialize($params['value'])) * 8;
+            if (is_string($params['value'])) {
+                $params['value'] = @unserialize($params['value']) ?: $params['value'];
+            }
             if ($this->collectValues) {
                 if ($this->isHtmlVarDumperUsed()) {
                     $params['value'] = $this->getVarDumper()->renderVar($params['value']);
