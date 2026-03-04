29a7ffc9288c28eba6334d051da9a6e2802aedeb
erikn69 <erikn_69@hotmail.com>
2026-03-04 16:55:17 -0500

    Add masked keys to ConfigCollector

--- src/CollectorProviders/ConfigCollectorProvider.php
+++ src/CollectorProviders/ConfigCollectorProvider.php
@@ -11,6 +11,8 @@ class ConfigCollectorProvider extends AbstractCollectorProvider
     public function __invoke(array $options): void
     {
         $configCollector = new ConfigCollector();
+        $masked = ['key', 'previous_keys'];
+        $configCollector->addMaskedKeys(array_merge($masked, $options['masked'] ?? []));
         $this->addCollector($configCollector);
     }
 }
