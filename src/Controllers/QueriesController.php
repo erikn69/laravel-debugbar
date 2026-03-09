ccd9f9eae96b8d5c07b92617f1ddde484a1c99c1
Barry vd. Heuvel <barryvdh@gmail.com>
2026-03-09 15:53:36 +0100

    Optin query result (#1997)
    * Optin query result
    
    * Update config
    
    * Tweak env
    
    * CS
    
    * Fix method call

--- src/Controllers/QueriesController.php
+++ src/Controllers/QueriesController.php
@@ -16,6 +16,23 @@ class QueriesController
      */
     public function explain(QueriesExplainRequest $request, LaravelDebugbar $debugbar, Explain $explain): \Illuminate\Http\JsonResponse
     {
+        $validated = $request->validated();
+
+        if (($validated['mode'] ?? null) === 'result') {
+
+            if (!config('debugbar.options.db.show_query_result', false) || !$debugbar->isStorageOpen($request)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Query result is currently disabled in the Debugbar.',
+                ], 400);
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => $explain->generateSelectResult($validated['connection'], $validated['query'], $validated['bindings'] ?? null, $validated['hash'], $validated['format'] ?? null),
+            ]);
+        }
+
         if (!config('debugbar.options.db.explain.enabled', false) || !$debugbar->isStorageOpen($request)) {
             return response()->json([
                 'success' => false,
@@ -23,20 +40,11 @@ class QueriesController
             ], 400);
         }
 
-        $validated = $request->validated();
-
         try {
             if (($validated['mode'] ?? null) === 'visual') {
                 return response()->json([
                     'success' => true,
                     'data' => $explain->generateVisualExplain($validated['connection'], $validated['query'], $validated['bindings'] ?? null, $validated['hash']),
-                ]);
-            }
-
-            if (($validated['mode'] ?? null) === 'result') {
-                return response()->json([
-                    'success' => true,
-                    'data' => $explain->generateSelectResult($validated['connection'], $validated['query'], $validated['bindings'] ?? null, $validated['hash'], $validated['format'] ?? null),
                 ]);
             }
 
