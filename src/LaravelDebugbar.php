1a317efb1caaaff0c06c716d3e6c6bad7627e026
Barry vd. Heuvel <barryvdh@gmail.com>
2026-04-10 09:23:23 +0200

    Refactor isStorageOpen method conditions to use canBeEnabled

--- src/LaravelDebugbar.php
+++ src/LaravelDebugbar.php
@@ -585,7 +585,7 @@ class LaravelDebugbar extends DebugBar
     public function isStorageOpen(Request $request): bool
     {
         // Additional safeguards that may never have storage open
-        if (!$this->isEnabled() || !config('app.debug') || (app()->isProduction() && !config('debugbar.force_allow_enable', false))) {
+        if (!static::canBeEnabled() || !$this->isEnabled()) {
             return false;
         }
 
